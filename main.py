4e590bdf95b00105311b515f302cdcc7cd1363de
Luxury-Karma <alexgauv@hotmail.ca>
2023-11-27 20:13:59 -0500

    can do playlist and text file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 from pytube import YouTube
+from pytube import Playlist
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import sys
 import argparse
@@ -21,18 +22,20 @@ def on_progress(stream, chunk, remaining):
 
 
 def download_video(video_url, output_path) -> str:
-    # Create a YouTube object
     yt = YouTube(video_url, on_progress_callback=on_progress)
-
-    # Get the highest resolution stream with both video and audio
-    video_file_path = yt.streams.filter(progressive=True, file_extension='mp4').desc().get_highest_resolution().download(output_path)
-
-    return video_file_path
+    print(f'\nThe video {yt.title} start is download\n')
+    if not os.path.isfile(f'{output_path}\\{yt.title}.mp3') or not os.path.isfile(f'{output_path}\\{yt.title}.mp3'):
+        video_file_path = yt.streams.filter(progressive=True, file_extension='mp4').desc().get_highest_resolution().download(output_path)
+        return video_file_path
+    print("This download was all ready in the directory.")
+    return "all ready Downloaded"
 
 
 def download_and_convert_to_mp3(video_url, output_path, del_mp4=True):
     try:
         video_file_path = download_video(video_url, output_path)
+        if video_file_path == "all ready Downloaded":
+            return
         print('downloaded videos\nStarting conversion to music')
         convert_video_to_mp3(video_file_path, output_path)
 
@@ -45,7 +48,7 @@ def download_and_convert_to_mp3(video_url, output_path, del_mp4=True):
 
 def convert_video_to_mp3(input_file, output_path):
     video = VideoFileClip(input_file)
-    video_title:str = video.filename
+    video_title: str = video.filename
     video_title = video_title.split('\\')[-1]
     video_title = video_title.strip('.mp4')
     output_file = os.path.join(output_path, f"{video_title}.mp3")
@@ -105,7 +108,30 @@ def download_one_music():
     download_and_convert_to_mp3(video_url=url,output_path=saving_directory)
 
 
-def download_multiple_video():
+def download_multiple_video_by_file():
+    file_path = input('Enter text file path: ')
+    file_data = ''
+    with open(file_path, 'r') as f:
+        file_data = f.read()
+    file_data = file_data.split('\n')
+    file_data = [line.strip() for line in file_data if line.strip()]
+    download_path = directory_sanitization('Where to download the content?: ')
+    downloaded_video = []
+    for e in file_data:
+        video_path = download_video(video_url=e, output_path=download_path)
+        if video_path == "all ready Downloaded":
+            continue
+        downloaded_video.append(video_path)
+
+    return download_path, downloaded_video
+
+def download_multiple_music_by_file():
+    output_path, video_path = download_multiple_video_by_file()
+    for e in video_path:
+        convert_video_to_mp3(e, output_path)
+        os.remove(e)
+
+def download_multiple_video_by_url():
     saving_directory = directory_sanitization("Enter the path to save content: ")
     url_to_download = []
     adding_link = True
@@ -118,12 +144,16 @@ def download_multiple_video():
     print('Starting the downloads')
     downloaded_video = []
     for e in url_to_download:
-        downloaded_video.append(download_video(e, saving_directory))
+        video_link = download_video(e, saving_directory)
+        if video_link == "all ready Downloaded":
+            continue
+        downloaded_video.append(video_link)
+
     return downloaded_video, saving_directory
 
 
 def download_multiple_music(del_mp4: bool = True):
-    music_path, output_path = download_multiple_video()
+    music_path, output_path = download_multiple_video_by_url()
     for e in music_path:
         convert_video_to_mp3(e, output_path)
         if del_mp4:
@@ -142,19 +172,44 @@ def user_menu_sanitisation(prompt:str, maximum_input:int):
         except:
             print(f"try again. The input wasn't an allowed number.\n it should be in between 1 and {maximum_input}")
 
+def download_playlist_video():
+    playlist_url = input('Enter the link to the playlist: ')
+    playlist = Playlist(playlist_url)
+    download_path = directory_sanitization('Enter where to download the playlist: ')
+    video_downloaded = []
+    for video in playlist.video_urls:
+        video_path = download_video(video, download_path)
+        if video_path == "all ready Downloaded":
+            continue
+        video_downloaded.append(video_path)
+    return download_path, video_downloaded
+
+def download_playlist_music():
+    output_path, video_path = download_playlist_video()
+    for e in video_path:
+        convert_video_to_mp3(e, output_path)
+        os.remove(e)
+
+
+
 
 def main_menue():
     menu = {
-        "1": ["download a video", download_one_video],
-        "2": ["download a music", download_one_music],
-        "3": ["download multiple videos", download_multiple_video],
-        "4": ["download multiple music", download_multiple_music]
+        "1": ["\033[92mdownload a video\033[0m", download_one_video],
+        "2": ["\033[92mdownload multiple videos\033[0m", download_multiple_video_by_url, '\033[94m'],
+        "3": ["\033[92mdownload videos from file\033[0m", download_multiple_video_by_file, '\033[94m'],
+        "4": ["\033[92mdownload videos from youtube playlist\033[0m", download_playlist_video, '\033[94m'],
+        "5": ["\033[94mdownload a music\033[0m", download_one_music],
+        "6": ["\033[94mdownload multiple music\033[0m", download_multiple_music, '\033[94m'],
+        "7": ["\033[94mdownload music from file\033[0m", download_multiple_music_by_file, '\033[94m'],
+        "8": ["\033[94mdownload musics from youtube playlist\033[0m", download_playlist_music],
+        "9": ["\033[91mquit\033[0m", quit]
     }
     menu_maximum_number = len(menu)
-    for e in menu:
-        print(f'{e}: {menu[e][0]}')
-
-    menu[user_menu_sanitisation(f"enter a number in between 1 and {menu_maximum_number}", menu_maximum_number)][1]()
+    while True:
+        for e in menu:
+            print(f'{e}: {menu[e][0]}')
+        menu[user_menu_sanitisation(f"enter a number in between 1 and {menu_maximum_number}: ", menu_maximum_number)][1]()
 
 
 if __name__ == "__main__":
