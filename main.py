1cafb135135fe3c3bd732a40c1c11781da82f05e
Luxury-Karma <alexgauv@hotmail.ca>
2023-11-27 19:00:26 -0500

    multi music download

--- main.py
+++ main.py
@@ -25,10 +25,7 @@ def download_video(video_url, output_path) -> str:
     yt = YouTube(video_url, on_progress_callback=on_progress)
 
     # Get the highest resolution stream with both video and audio
-    video_stream = yt.streams.get_highest_resolution()
-
-    # Download the video
-    video_file_path = video_stream.download(output_path)
+    video_file_path = yt.streams.filter(progressive=True, file_extension='mp4').desc().get_highest_resolution().download(output_path)
 
     return video_file_path
 
@@ -36,7 +33,7 @@ def download_video(video_url, output_path) -> str:
 def download_and_convert_to_mp3(video_url, output_path, del_mp4=True):
     try:
         video_file_path = download_video(video_url, output_path)
-
+        print('downloaded videos\nStarting conversion to music')
         convert_video_to_mp3(video_file_path, output_path)
 
         print("\nConversion completed successfully.")
@@ -48,7 +45,10 @@ def download_and_convert_to_mp3(video_url, output_path, del_mp4=True):
 
 def convert_video_to_mp3(input_file, output_path):
     video = VideoFileClip(input_file)
-    output_file = os.path.join(output_path, "output_audio.mp3")
+    video_title:str = video.filename
+    video_title = video_title.split('\\')[-1]
+    video_title = video_title.strip('.mp4')
+    output_file = os.path.join(output_path, f"{video_title}.mp3")
     video.audio.write_audiofile(output_file)
     video.close()
 
@@ -106,22 +106,30 @@ def download_one_music():
 
 
 def download_multiple_video():
-    saving_directory = directory_sanitization("enter where you want the videos downloaded")
+    saving_directory = directory_sanitization("Enter the path to save content: ")
     url_to_download = []
     adding_link = True
     while adding_link:
-        url = youtube_url_sanitization('Enter the youtube URL')
+        url = youtube_url_sanitization('Enter the youtube URL to download: ')
         if url == 'quit':
             adding_link = False
             continue
         url_to_download.append(url)
+    print('Starting the downloads')
+    downloaded_video = []
     for e in url_to_download:
-        command = ['python', 'main.py', '-u', e, '-o', saving_directory, '-mp34', 'mp4']
-        subprocess.run(command)
+        downloaded_video.append(download_video(e, saving_directory))
+    return downloaded_video, saving_directory
 
-def download_multiple_music():
-    saving_directory = directory_sanitization("enter where you want the musics downloaded")
-    url = youtube_url_sanitization('Enter the youtube URL')
+
+def download_multiple_music(del_mp4: bool = True):
+    music_path, output_path = download_multiple_video()
+    for e in music_path:
+        convert_video_to_mp3(e, output_path)
+        if del_mp4:
+            os.remove(e)
+
+
 
 
 def user_menu_sanitisation(prompt:str, maximum_input:int):
@@ -130,7 +138,7 @@ def user_menu_sanitisation(prompt:str, maximum_input:int):
         try:
             u_input = int(u_input)
             if u_input >= 1 and u_input <= maximum_input:
-                return u_input
+                return f'{u_input}'
         except:
             print(f"try again. The input wasn't an allowed number.\n it should be in between 1 and {maximum_input}")
 
@@ -146,7 +154,7 @@ def main_menue():
     for e in menu:
         print(f'{e}: {menu[e][0]}')
 
-    menu[user_menu_sanitisation(f"enter a number in between 1 and {menu_maximum_number}", f"{menu_maximum_number}")][1]()
+    menu[user_menu_sanitisation(f"enter a number in between 1 and {menu_maximum_number}", menu_maximum_number)][1]()
 
 
 if __name__ == "__main__":
