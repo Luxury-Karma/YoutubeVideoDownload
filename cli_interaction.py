56cfa6f7fd50ce87ef693b47501bdb170439fd3e
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2023-11-28 10:45:50 -0500

    clean up

--- cli_interaction.py
+++ cli_interaction.py
@@ -1,6 +1,8 @@
 from main import *
+import os
 
-def user_menu_sanitisation(prompt:str, maximum_input:int):
+
+def user_menu_sanitisation(prompt: str, maximum_input: int):
     while True:
         u_input = input(prompt)
         try:
@@ -10,10 +12,12 @@ def user_menu_sanitisation(prompt:str, maximum_input:int):
         except:
             print(f"try again. The input wasn't an allowed number.\n it should be in between 1 and {maximum_input}")
 
+
 def download_one_video():
     saving_directory = directory_sanitization("enter where you want the video downloaded")
     url = youtube_url_sanitization('Enter the youtube URL')
     download_video(video_url=url, output_path=saving_directory)
+
 
 def download_multiple_video_by_url():
     saving_directory = directory_sanitization("Enter the path to save content: ")
@@ -35,42 +39,20 @@ def download_multiple_video_by_url():
 
     return downloaded_video, saving_directory
 
-def download_multiple_video_by_file():
-    file_path = input('Enter text file path: ')
-    file_data = ''
-    with open(file_path, 'r') as f:
-        file_data = f.read()
-    file_data = file_data.split('\n')
-    file_data = [line.strip() for line in file_data if line.strip()]
-    download_path = directory_sanitization('Where to download the content?: ')
-    downloaded_video = []
-    for e in file_data:
-        video_path = download_video(video_url=e, output_path=download_path)
-        if video_path == "all ready Downloaded":
-            continue
-        downloaded_video.append(video_path)
 
-    return download_path, downloaded_video
+def download_multiple_video_by_file():
+    download_video_from_file(input('Enter text file path: '),directory_sanitization('Enter the download path'))
+
 
 def download_playlist_video():
-    playlist_url = input('Enter the link to the playlist: ')
-    playlist = Playlist(playlist_url)
-    download_path = directory_sanitization('Enter where to download the playlist: ')
-    video_downloaded = []
-    total_video_amount = len(playlist.video_urls)
-    total_video_downloaded = 0
-    for video in playlist.video_urls:
-        print(f'\033[93mDownload complet: {total_video_downloaded}/{total_video_amount}\033[0m')
-        video_path = download_video(video, download_path)
-        if video_path == "all ready Downloaded":
-            continue
-        video_downloaded.append(video_path)
-    return download_path, video_downloaded
+    playlist_download(input('Enter the link to the playlist: '),directory_sanitization('Enter where to download the playlist: '))
+
 
 def download_one_music():
     saving_directory = directory_sanitization("enter where you want the music downloaded")
     url = youtube_url_sanitization('Enter the youtube URL')
     download_and_convert_to_mp3(video_url=url, output_path=saving_directory)
+
 
 def download_multiple_music(del_mp4: bool = True):
     music_path, output_path = download_multiple_video_by_url()
@@ -78,6 +60,7 @@ def download_multiple_music(del_mp4: bool = True):
         convert_video_to_mp3(e, output_path)
         if del_mp4:
             os.remove(e)
+
 
 def download_multiple_music_by_file():
     output_path, video_path = download_multiple_video_by_file()
@@ -93,12 +76,12 @@ def download_playlist_music():
         os.remove(e)
 
 
-def main_menue():
+def main_menu():
     menu = {
         "1": ["\033[92mdownload a \033[91myoutube\033[92m video\033[0m", download_one_video],
         "2": ["\033[92mdownload multiple \033[91myoutube\033[92m videos\033[0m", download_multiple_video_by_url, '\033[94m'],
         "3": ["\033[92mdownload \033[91myoutube\033[92m videos from file\033[0m", download_multiple_video_by_file, '\033[94m'],
-        "4": ["\033[92mdownload youtube videos from \033[92myoutube\033[94m playlist\033[0m", download_playlist_video, '\033[94m'],
+        "4": ["\033[92mdownload youtube videos from \033[92myoutube\033[92m playlist\033[0m", download_playlist_video, '\033[94m'],
         "5": ["\033[94mdownload a music on \033[91myoutube\033[0m", download_one_music],
         "6": ["\033[94mdownload multiple music on \033[91myoutube\033[0m", download_multiple_music, '\033[94m'],
         "7": ["\033[94mdownload music on \033[91myoutube\033[94m from file\033[0m", download_multiple_music_by_file, '\033[94m'],
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import os
-from pytube import YouTube
-from pytube import Playlist
-from moviepy.video.io.VideoFileClip import VideoFileClip
-import sys
 import argparse
 import re
+import sys
+
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
 from cli_interaction import *
+from pytube import YouTube
+from pytube import Playlist
 
 
 def on_progress(stream, chunk, remaining):
@@ -20,13 +21,18 @@ def on_progress(stream, chunk, remaining):
 
 
 def download_video(video_url, output_path) -> str:
-    yt = YouTube(video_url, on_progress_callback=on_progress)
-    print(f'\nThe video {yt.title} start is download\n')
-    if not os.path.isfile(f'{output_path}\\{yt.title}.mp3') or not os.path.isfile(f'{output_path}\\{yt.title}.mp3'):
-        video_file_path = yt.streams.filter(progressive=True, file_extension='mp4').desc().get_highest_resolution().download(output_path)
-        return video_file_path
-    print("This download was all ready in the directory.")
-    return "all ready Downloaded"
+    try:
+        yt = YouTube(video_url, on_progress_callback=on_progress)
+        print(f'\nThe video {yt.title} start is download\n')
+        if not os.path.isfile(f'{output_path}\\{yt.title}.mp3') or not os.path.isfile(f'{output_path}\\{yt.title}.mp3'):
+            video_file_path = yt.streams.filter(progressive=True,
+                                                file_extension='mp4').desc().get_highest_resolution().download(
+                output_path)
+            return video_file_path
+        print("This download was all ready in the directory.")
+        return "all ready Downloaded"
+    except:
+        return "all ready Downloaded"
 
 
 def download_and_convert_to_mp3(video_url, output_path, del_mp4=True):
@@ -41,26 +47,75 @@ def download_and_convert_to_mp3(video_url, output_path, del_mp4=True):
         if del_mp4:
             os.remove(video_file_path)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: \033[91m{e}\033[0m")
+
+
+def playlist_download(playlist_url: str, download_path: str):
+    try:
+        playlist = Playlist(playlist_url)
+    except Exception as e:
+        print(f'Playlist link error.\n \033[91mERROR:{e}\033[0m')
+        return
+    video_downloaded = []
+
+    total_video_amount = len(playlist.video_urls)
+    total_video_downloaded = 0
+
+    for video in playlist.video_urls:
+        print(f'\033[93mDownload complete: {total_video_downloaded}/{total_video_amount}\033[0m')
+        video_path = download_video(video, download_path)
+        if video_path == "all ready Downloaded":
+            total_video_downloaded = total_video_downloaded + 1
+            continue
+        video_downloaded.append(video_path)
+        total_video_downloaded = total_video_downloaded + 1
+    return download_path, video_downloaded
 
 
 def convert_video_to_mp3(input_file, output_path):
-    video = VideoFileClip(input_file)
-    video_title: str = video.filename
-    video_title = video_title.split('\\')[-1]
-    video_title = video_title.strip('.mp4')
-    output_file = os.path.join(output_path, f"{video_title}.mp3")
-    video.audio.write_audiofile(output_file)
-    video.close()
+    try:
+        video = VideoFileClip(input_file)
+        video_title: str = video.filename
+        video_title = video_title.split('\\')[-1]
+        video_title = video_title.strip('.mp4')
+        output_file = os.path.join(output_path, f"{video_title}.mp3")
+        video.audio.write_audiofile(output_file)
+        video.close()
+        return
+    except Exception as e:
+        print(
+            f'\033[91mError with file: {input_file} the file will not be created.\n\033[91mERROR CODE : {e} the program will continue\033[0m')
+        return
+
+
+def download_video_from_file(input_file: str, ouput_path):
+    try:
+        with open(input_file, 'r') as f:
+            file_data = f.read()
+        file_data = file_data.split('\n')
+        file_data = [line.strip() for line in file_data if line.strip()]
+        downloaded_video = []
+    except Exception as e:
+        print(f'Error with the file opening or format.\n\033[91mERROR : {e}\033[0m')
+        return
+    for e in file_data:
+        video_path = download_video(video_url=e, output_path=ouput_path)
+        if video_path == "all ready Downloaded":
+            continue
+        downloaded_video.append(video_path)
+
+    return ouput_path, downloaded_video
 
 
 def argument_handler():
     parser = argparse.ArgumentParser(description='A youtube downloader script')
-    parser.add_argument('-u', '--url', help='Url to the video you want to download on youtube',  type=str)
+    parser.add_argument('-u', '--url', help='Url to the video you want to download on youtube', type=str)
     parser.add_argument('-pl', '--playlist', help='Enter the URL of the playlist to download', type=str)
     parser.add_argument('-tl', '--textlist', help='Enter a text file with URL to download', type=str)
-    parser.add_argument('-mp', '--mp34', help='Output file path', type=str, default='mp4', required='-u' in sys.argv or '-pl' in sys.argv or '-tl' in sys.argv)
-    parser.add_argument('-o', '--output', help='path to the output file', type=str, default=os.getcwd(), required='-u' in sys.argv or '-pl' in sys.argv or '-tl' in sys.argv)
+    parser.add_argument('-mp', '--mp34', help='Output file path', type=str, default='mp4',
+                        required='-u' in sys.argv or '-pl' in sys.argv or '-tl' in sys.argv)
+    parser.add_argument('-o', '--output', help='path to the output file', type=str, default=os.getcwd(),
+                        required='-u' in sys.argv or '-pl' in sys.argv or '-tl' in sys.argv)
     parser.add_argument('-p', '--program', help='run as it was a program with a menu', type=str, default=True)
 
     return parser.parse_args()
@@ -96,13 +151,6 @@ def youtube_url_sanitization(prompt: str):
             raise ValueError(f"{input_url} is not a valid YouTube video URL.")
 
 
-
-
-
-
-
-
-
 if __name__ == "__main__":
     args = argument_handler()
     if args.url:
@@ -112,18 +160,23 @@ if __name__ == "__main__":
             download_and_convert_to_mp3(args.url, args.output)
     elif args.textlist:
         if args.mp34 == 'mp4':
-            pass
+            download_video_from_file(args.textlist, args.output)
         if args.mp34 == 'mp3':
-            pass
+            output_path, video_path = download_video_from_file(args.textlist, args.output)
+            for e in video_path:
+                convert_video_to_mp3(e, output_path)
+                os.remove(e)
         else:
             print('The mp34 is either mp3 or mp4.\n\033[91mexit\033[0m')
     elif args.playlist:
         if args.mp34 == 'mp4':
-            pass
+            playlist_download(args.playlist, args.output)
         if args.mp34 == 'mp3':
-            pass
+            output_path, video_path = playlist_download(args.playlist, args.output)
+            for e in video_path:
+                convert_video_to_mp3(e, output_path)
+                os.remove(e)
         print('The mp34 is either mp3 or mp4.\n\033[91mexit\033[0m')
 
     else:
-        main_menue()  # launch program mode
-
+        main_menu()  # launch program mode
